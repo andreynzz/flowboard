2c6bb9902ffc605c0ae0dd871652f4db9939c4ca
Andrey <andrey.dhorapirola@gmail.com>
2026-05-15 15:44:57 -0300

    refactor: migrate auth middleware to proxy

---
+++ proxy.ts
@@ -0,0 +1,19 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+  function middleware(req) {
+    return;
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+    pages: {
+      signIn: "/login",
+    },
+  }
+);
+
+export const config = {
+  matcher: ["/app/:path*"],
+};
